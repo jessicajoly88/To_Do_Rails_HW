f877f9e1610155d272fa23d687d184c3ecb98afd
Jessica <jessicajoly88@gmail.com>
2015-10-11 12:03:02 -0700

    Add controller and view for details list_id

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,5 +2,10 @@ class ListsController < ApplicationController
   def index
   	@lists = List.all
   	render :index
-  end	
+  end
+
+  def show 
+    @list = List.find(params[:id])
+    render :show
+   end 
 end
